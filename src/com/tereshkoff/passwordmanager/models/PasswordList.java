7c4036d96e9660dae4e73d92c92bd8238ab01ad9
handioq <ter.alexanderandr@gmail.com>
2016-03-13 18:08:08 +0200

    pwlist and pw update

--- src/com/tereshkoff/passwordmanager/models/PasswordList.java
+++ src/com/tereshkoff/passwordmanager/models/PasswordList.java
@@ -1,12 +1,29 @@
 package com.tereshkoff.passwordmanager.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PasswordList {
+
+    private List<Password> passwordList = new ArrayList<Password>();
+    private String name;
 
     public PasswordList()
     {
 
     }
 
+    public PasswordList(String name, List<Password> passwordList)
+    {
+        this.passwordList = passwordList;
+        this.name = name;
+    }
 
+    public List<Password> getPasswordList() {
+        return passwordList;
+    }
 
+    public String getName() {
+        return name;
+    }
 }
